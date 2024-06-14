56de0aa57674933a13184aad520014ce7a222301
Takuya Uehara <bb38122012@ms.nagasaki-u.ac.jp>
2024-06-15 02:23:07 +0900

    ヘッダー部分の実装

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <h3>Portfolio</h3>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">SNS</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
